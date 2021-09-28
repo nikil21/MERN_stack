c27826bae6bb73afe7001d1d81cd762b422084f9
nikil21 <nikilesh21@gmail.com>
2021-09-28 19:52:00 +0530

    apps

---
+++ Oneshot/backend/api/colleges.route.js
@@ -0,0 +1,12 @@
+import express from "express"
+import CollegesCtrl from "./colleges.controller.js"
+
+const router = express.Router()
+
+router.route("/").get(CollegesCtrl.apiGetColleges);
+router.route("/id/:id").get(CollegesCtrl.apiGetCollegeById)
+// router.route("/id/:id").get(CollegesCtrl.apiGetSimilarColleges)
+router.route("/Courses").get(CollegesCtrl.apiGetCollegeCourses)
+
+
+export default router
